c9a80a31b4c4633ecd03738680283de9a592e518
Antonio Galisteo <antonio.galisteo@thoughtworks.com>
2022-03-25 09:38:38 -0500

    Clean Code Example 1

---
+++ src/com/programandotv/Main.java
@@ -0,0 +1,9 @@
+package com.programandotv;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Utils utilities = new Utils();
+        System.out.println(utilities.calculate(8.0f,15.2f));
+    }
+}
---
+++ src/com/programandotv/Utils.java
@@ -0,0 +1,8 @@
+package com.programandotv;
+
+public class Utils {
+
+    public float calculate(float a, float b) {
+        return a*b;
+    }
+}
